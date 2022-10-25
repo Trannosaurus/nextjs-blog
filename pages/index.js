2fd19856d52f80c166df47bab6b9f6a1fa2e3ac2
Trannosaurus <AnT.Tran2003@gmail.com>
2022-10-25 07:34:56 -0700

    bio

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,19 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm <strong>An</strong>, a sophmore and the University of Maryland College Park</p>
+        <p>Hi, I'm <strong>An</strong>, a sophmore at the University of Maryland College Park.
+        I have experience with Express, React, NextJS, typescript, Tailwind CSS, MongoBD, 
+        javascript, html, css, java, and C, and I am currently learning prisma, mySQL, TRPC
+        and looking forward to learning more!</p>
+        <br></br>
+        <p>Wanting to help out at my parents nail salon, I built a full stack 
+          app where users or clients could book appointments with nail technicians
+          because I saw that the process they had in place for booking appointments
+          was too choatic and could be automized. I used the T3 tech stack 
+          (nextjs, tRPC, tailwind, typescript, prisma) and managed user authentification
+          and authorization and encryption. You can read more about that 
+          <Link href="/posts/pre-rendering.md-post">here!</Link>
+        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
