97ffe58cc4ce18bdfcc89b64284ac7b462e8bfd3
Trannosaurus <AnT.Tran2003@gmail.com>
2022-10-24 19:51:07 -0700

    pfp

--- pages/index.js
+++ pages/index.js
@@ -23,10 +23,6 @@ export default function Home({allPostsData}) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi, I'm <strong>An</strong>, a sophmore and the University of Maryland College Park</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
