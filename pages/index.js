faf6b701b14e92d48577e794180e5f2edef89cdb
An Tran <AnT.Tran2003@gmail.com>
2023-08-18 09:44:51 -0400

    video

--- pages/index.js
+++ pages/index.js
@@ -22,11 +22,14 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi, I'm <strong>An</strong>, a sophmore at the University of Maryland College Park.
-        I have experience with Express, React, NextJS, typescript, Tailwind CSS, MongoBD, 
-        javascript, html, css, java, and C, and I am currently learning prisma, mySQL, TRPC
-        and looking forward to learning more!</p>
+        Hey, I'm <strong>An Tran</strong>, a passionate computer science student with a knack for turning ideas into reality. Armed with a solid academic foundation and hands-on experience, I've honed my skills in algorithms, web development, and computer systems. As a Lead Frontend Developer at Sociable AI, I've driven user engagement through automation and optimized web app performance using Next.js, Tailwind, and React. My innovative spirit shines in projects like AnonPlay—a WebRTC-powered Single Page Application that hosts private watch parties, emphasizing real-time interactions.
       </section>
+      <section className={utilStyles.headingMd}>
+        My expertise spans languages like OCaml and frameworks like Next.js, culminating in creations like MicroCaml—a testament to my grasp of dynamic languages. Beyond coding, I thrive in collaborative environments. At Nail Art & Spa, I spearheaded a full-stack Next.js app, streamlining booking processes and skillfully managing user data. I'm not just a developer; I'm a problem solver, an innovator, and a valuable team player. Join me as I continue to explore new horizons and craft remarkable digital experiences.
+      </section>
+      <iframe width="610" height="350"
+        src="https://www.youtube.com/embed/zmFZA_wiAf4">
+      </iframe>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
